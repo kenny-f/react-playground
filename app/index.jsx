4698025712460f3de4a1013952ef61bddfe4c604
Kenny Fung <Kenny.Fung@thetrainline.com>
2015-12-12 20:47:29 +0000

    added dashboard

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {Router, Route } from 'react-router';
 
 import Component from './Component.jsx';
 import Navigation from './Navigation';
+import Home from './Home';
+import Dashboard from './Dashboard';
 
-render(<Navigation />, document.getElementById('root'));
+// render(<Navigation />, document.getElementById('root'));
+
+render(
+  (
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Router>
+  ),
+  document.getElementById('root')
+);
