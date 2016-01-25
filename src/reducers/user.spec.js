b1679341b43c373f49e25a52354256c7280e7efa
Kenny Fung <Kenny.Fung@thetrainline.com>
2016-01-25 20:27:12 +0000

    changed tests to use tape over mocha

--- src/reducers/user.spec.js
+++ src/reducers/user.spec.js
@@ -1,62 +1,62 @@
+import test from 'tape';
 import Immuatable from 'seamless-immutable';
-import { expect } from 'chai';
 
 import userReducer from './user';
 
-describe('user reducer', () => {
-  it('it has initial state', () => {
-    const nextState = userReducer(undefined, {});
-    const expectedState = Immuatable({
-      isFetching: false,
-      isAuthenticated: false,
-    });
-    expect(nextState).to.deep.equal(expectedState);
+test('user reducer with undefined state', (assert) => {
+  const nextState = userReducer(undefined, {});
+  const expectedState = Immuatable({
+    isFetching: false,
+    isAuthenticated: false,
   });
+  assert.deepEqual(nextState, expectedState, 'should have initial state');
+  assert.end();
+});
 
-  it('handles LOGIN_REQUEST', () => {
-    const creds = { email: '123' };
-    const action = {
-      type: 'LOGIN_REQUEST',
-      creds,
-    };
-    const expectedState = Immuatable({
-      isFetching: true,
-      isAuthenticated: false,
-      creds,
-    });
-    const nextState = userReducer(undefined, action);
-
-    expect(nextState).to.deep.equal(expectedState);
+test('user reducer gets a LOGIN_REQUEST action', assert => {
+  const creds = { email: '123' };
+  const action = {
+    type: 'LOGIN_REQUEST',
+    creds,
+  };
+  const expectedState = Immuatable({
+    isFetching: true,
+    isAuthenticated: false,
+    creds,
   });
+  const nextState = userReducer(undefined, action);
 
-  it('handles LOGIN_SUCCESS', () => {
-    const user = { token: '123' };
-    const action = {
-      type: 'LOGIN_SUCCESS',
-      user,
-    };
-    const expectedState = Immuatable({
-      isFetching: false,
-      isAuthenticated: true,
-      user,
-    });
-    const nextState = userReducer(undefined, action);
+  assert.deepEqual(nextState, expectedState, 'it updates the state');
+  assert.end();
+});
 
-    expect(nextState).to.deep.equal(expectedState);
+test('user reducer gets a LOGIN_SUCCESS action', assert => {
+  const user = { token: '123' };
+  const action = {
+    type: 'LOGIN_SUCCESS',
+    user,
+  };
+  const expectedState = Immuatable({
+    isFetching: false,
+    isAuthenticated: true,
+    user,
   });
+  const nextState = userReducer(undefined, action);
+  assert.deepEqual(nextState, expectedState, 'it updates the state');
+  assert.end();
+});
 
-  it('handles LOGIN_FAILURE', () => {
-    const action = {
-      type: 'LOGIN_FAILURE',
-      message: 'error message',
-    };
-    const expectedState = Immuatable({
-      isFetching: false,
-      isAuthenticated: false,
-      message: action.message,
-    });
-    const nextState = userReducer(undefined, action);
-
-    expect(nextState).to.deep.equal(expectedState);
+test('user reducer gets a LOGIN_FAILURE action', assert => {
+  const action = {
+    type: 'LOGIN_FAILURE',
+    message: 'error message',
+  };
+  const expectedState = Immuatable({
+    isFetching: false,
+    isAuthenticated: false,
+    message: action.message,
   });
+  const nextState = userReducer(undefined, action);
+  assert.deepEqual(nextState, expectedState, 'it updates the state');
+  assert.end();
 });
