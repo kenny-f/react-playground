0049786d255ada6a8e8aa3c97306f9c2a4c024de
Kenny Fung <Kenny.Fung@thetrainline.com>
2016-01-26 17:02:13 +0000

    moved back to mocha as there is better tooling

--- src/reducers/user.spec.js
+++ src/reducers/user.spec.js
@@ -1,62 +1,60 @@
-import test from 'tape';
+import assert from 'assert';
 import Immuatable from 'seamless-immutable';
 
 import userReducer from './user';
 
-test('user reducer with undefined state', (assert) => {
-  const nextState = userReducer(undefined, {});
-  const expectedState = Immuatable({
-    isFetching: false,
-    isAuthenticated: false,
+describe('user reducer should', () => {
+  it('have inital state', () => {
+    const nextState = userReducer(undefined, {});
+    const expectedState = Immuatable({
+      isFetching: false,
+      isAuthenticated: false,
+    });
+    assert.deepEqual(nextState, expectedState);
   });
-  assert.deepEqual(nextState, expectedState, 'should have initial state');
-  assert.end();
-});
 
-test('user reducer gets a LOGIN_REQUEST action', assert => {
-  const creds = { email: '123' };
-  const action = {
-    type: 'LOGIN_REQUEST',
-    creds,
-  };
-  const expectedState = Immuatable({
-    isFetching: true,
-    isAuthenticated: false,
-    creds,
+  it('user reducer gets a LOGIN_REQUEST action', () => {
+    const creds = { email: '123' };
+    const action = {
+      type: 'LOGIN_REQUEST',
+      creds,
+    };
+    const expectedState = Immuatable({
+      isFetching: true,
+      isAuthenticated: false,
+      creds,
+    });
+    const nextState = userReducer(undefined, action);
+
+    assert.deepEqual(nextState, expectedState);
   });
-  const nextState = userReducer(undefined, action);
 
-  assert.deepEqual(nextState, expectedState, 'it updates the state');
-  assert.end();
-});
-
-test('user reducer gets a LOGIN_SUCCESS action', assert => {
-  const user = { token: '123' };
-  const action = {
-    type: 'LOGIN_SUCCESS',
-    user,
-  };
-  const expectedState = Immuatable({
-    isFetching: false,
-    isAuthenticated: true,
-    user,
+  it('user reducer gets a LOGIN_SUCCESS action', () => {
+    const user = { token: '123' };
+    const action = {
+      type: 'LOGIN_SUCCESS',
+      user,
+    };
+    const expectedState = Immuatable({
+      isFetching: false,
+      isAuthenticated: true,
+      user,
+    });
+    const nextState = userReducer(undefined, action);
+    assert.deepEqual(nextState, expectedState);
   });
-  const nextState = userReducer(undefined, action);
-  assert.deepEqual(nextState, expectedState, 'it updates the state');
-  assert.end();
-});
 
-test('user reducer gets a LOGIN_FAILURE action', assert => {
-  const action = {
-    type: 'LOGIN_FAILURE',
-    message: 'error message',
-  };
-  const expectedState = Immuatable({
-    isFetching: false,
-    isAuthenticated: false,
-    message: action.message,
+  it('user reducer gets a LOGIN_FAILURE action', () => {
+    const action = {
+      type: 'LOGIN_FAILURE',
+      message: 'error message',
+    };
+    const expectedState = Immuatable({
+      isFetching: false,
+      isAuthenticated: false,
+      message: action.message,
+    });
+    const nextState = userReducer(undefined, action);
+    assert.deepEqual(nextState, expectedState);
   });
-  const nextState = userReducer(undefined, action);
-  assert.deepEqual(nextState, expectedState, 'it updates the state');
-  assert.end();
 });
--- src/sagas/login-saga.spec.js
+++ src/sagas/login-saga.spec.js
@@ -1,31 +1,37 @@
 import 'babel-polyfill';
-import test from 'tape';
+import assert from 'assert';
 import { take, call, put } from 'redux-saga';
+
 import loginSaga from './login-saga';
 import { login } from '../api';
 import { receiveLogin } from '../actions/actions';
 
-test('login saga', assert => {
-  const saga = loginSaga();
+const saga = loginSaga();
 
-  let actual = null;
-  let expected = null;
+let actual = null;
+let expected = null;
 
-  actual = saga.next().value;
-  expected = take('LOGIN_REQUEST');
-  assert.deepEqual(actual, expected, 'waits for LOGIN_REQUEST action');
+describe('login saga should', () => {
+  it('wait for LOGIN_REQUEST action', () => {
+    actual = saga.next().value;
+    expected = take('LOGIN_REQUEST');
+    assert.deepEqual(actual, expected);
+  });
 
-  actual = saga.next().value;
-  const loginResponse = actual;
-  expected = call(login);
-  assert.deepEqual(actual, expected, 'calls login api');
-
-  actual = saga.next().value;
-  expected = put(receiveLogin());
-  assert.deepEqual(actual, expected, 'dispatch login success action');
-
-  actual = saga.next().done;
-  expected = true;
-  assert.deepEqual(actual, expected, 'finished');
-  assert.end();
+  it('call the login api', () => {
+    actual = saga.next().value;
+    const loginResponse = actual;
+    expected = call(login);
+    assert.deepEqual(actual, expected);
+  });
+  it('dispatch login success action', () => {
+    actual = saga.next().value;
+    expected = put(receiveLogin());
+    assert.deepEqual(actual, expected);
+  });
+  it('be done', () => {
+    actual = saga.next().done;
+    expected = true;
+    assert.deepEqual(actual, expected, 'finished');
+  });
 });
