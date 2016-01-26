aa4cdb6199559a7d5efd01a5b1a15c48c01303e7
Kenny Fung <Kenny.Fung@thetrainline.com>
2016-01-26 16:17:20 +0000

    cleanup

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,5 @@ import 'isomorphic-fetch';
 export function login() {
   return fetch('http://localhost:3001/login')
     .then(res => res.json())
-    .then(function (json) {
-      return json;
-    });
+    .then(json => json);
 }
